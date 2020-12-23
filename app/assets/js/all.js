1530d5a4843e01b48f2faa45237a45d971ba494a
alysachan <alysachansl@gmail.com>
2020-12-23 15:16:30 +0800

    Finish search bar in PC version

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,31 @@
-$(function() {
-  console.log('Hello Bootstrap4');
+// Swiper in index.html
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
+
+// jQuery
+$(document).ready( () => {
+  // Date range picker in index.html
+  $(function() {
+
+    $('input[name="datefilter"]').daterangepicker({
+        autoUpdateInput: false,
+        locale: {
+            cancelLabel: 'Clear'
+        }
+    });
+  
+    $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    });
+  
+    $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+    });
+  
+  });
+})
